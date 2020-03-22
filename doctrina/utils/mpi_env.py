1e1eb2de07f01c8e1d6b83067a3a37d446d07eea
TolgaOk <tolgaokk@gmail.com>
2020-03-22 21:06:22 +0300

    MPI environment added

---
+++ doctrina/utils/mpi_env.py
@@ -0,0 +1,270 @@
+import gym
+from copy import deepcopy
+from mpi4py import MPI
+import numpy as np
+
+from mpi_utils import list_gather_and_scatter, gather_and_scatter
+
+
+class MpiEnvComm():
+    """ Base communication class for MPI environments.
+    """
+
+    def __init__(self, n_env_proc):
+        self.n_env_proc = n_env_proc
+        size = MPI.COMM_WORLD.Get_size()
+        rank = MPI.COMM_WORLD.Get_rank()
+
+        if size <= n_env_proc:
+            raise ValueError(
+                "Not enough processes for {} many environments"
+                .format(n_env_proc))
+
+        color = MpiEnvComm.get_color(n_env_proc)
+        self.local_comm = MPI.COMM_WORLD.Split(color=color, key=rank)
+        self.color = color
+        remote_leader = 0 if color == 1 else size - n_env_proc
+
+        self.remote_comm = self.local_comm.Create_intercomm(
+            local_leader=0,
+            peer_comm=MPI.COMM_WORLD,
+            remote_leader=remote_leader,
+            tag=12)
+
+    @staticmethod
+    def get_color(n_env_proc):
+        size = MPI.COMM_WORLD.Get_size()
+        rank = MPI.COMM_WORLD.Get_rank()
+        return 1 if size - rank <= n_env_proc else 0
+
+    def render(self):
+        raise NotImplementedError
+
+    def reset(self):
+        raise NotImplementedError
+
+
+class MpiEnvClient(MpiEnvComm):
+    """ Parallel environment class that utilize MPI functionallity.
+    """
+
+    def __init__(self, nenv, n_env_proc, env_fn, **env_kwargs):
+        super().__init__(n_env_proc)
+        self.nenv = nenv
+
+        env = env_fn(**env_kwargs)
+
+        if not isinstance(env.action_space, gym.spaces.discrete.Discrete):
+            raise ValueError("Only discrete actions are supported!")
+
+        self.observation_space_shape = env.observation_space.shape
+        self.observation_space_dtype = env.observation_space.dtype
+        self.action_space_size = env.action_space.n
+        self.action_space_dtype = env.action_space.dtype
+
+    def send_cmd(self, command):
+        """ Inform the worker(environment) processes to wait for a specific
+        job. Available commands are as follows:
+            - s_state: Used for observe method to inform environments that
+            gpu processes will be asking for state information.
+            - s_state_r_action: Receive actions from gpu and send states
+            - s_trans_r_action: Receive actions from gpu and send transition
+            triplet.
+            - s_trans: Send transition triplet to gpu.
+            - switch_envs: Tell environments to switch the second group of
+            environments. Used for **pipelining**.
+            - terminate: Terminate environment processes.
+        Worker processes always wait for a command after performing a task.
+        """
+        cmd_list = ["s_state", "s_state_r_action", "s_trans_r_action",
+                    "s_trans", "switch_envs", "terminate"]
+        if isinstance(command, str):
+            if command not in cmd_list:
+                raise ValueError("Given string command is not available")
+            command = cmd_list.index(command)
+        self.remote_comm.bcast({"command": command}, MPI.ROOT)
+
+    def observe(self, actions=None):
+        """ Gather and distribute states from environment to gpus nodes.
+        If the "actions" is
+        """
+        if (self.nenv * self.n_env_proc) % self.local_comm.Get_size() != 0:
+            raise RuntimeError(
+                ("#environment per env processs times the #env process must"
+                 "be an exact multiple of #gpu processes. {} x {} % {}")
+                .format(self.nenv, self.n_env_proc, self.local_comm.Get_size()))
+
+        if actions is not None:
+            if len(actions.shape) != 2:
+                raise ValueError("Action must be 2 dimensional. (B x k)")
+
+        # Send empty buffer to environment processes
+        if actions is None:
+            sendbuf = np.empty(0)
+            self.send_cmd("s_state")
+        else:
+            sendbuf = actions
+            self.send_cmd("s_state_r_action")
+
+        states = gather_and_scatter(self.local_comm, sendbuf, self.remote_comm)
+
+        self.send_cmd("switch_envs")
+
+        return states
+
+    def step(self, actions=None):
+        """ Distribute actions to environments to step. Gather transition
+        triplets.
+        Arguments:
+            - actions: (B x k) where k is the number of actions per sample.
+            "k" is 1 for discrete action spaces.
+        """
+
+        if actions is not None:
+            if len(actions.shape) != 2:
+                raise ValueError("Action must be 2 dimensional. (B x k)")
+
+        # Send action buffer to environment processes
+        if actions is None:
+            sendbuf = np.empty(0)
+            self.send_cmd("s_trans")
+        else:
+            sendbuf = actions
+            self.send_cmd("s_trans_r_action")
+
+        transition_triplet = list_gather_and_scatter(
+            self.local_comm, [sendbuf], self.remote_comm)
+
+        self.send_cmd("switch_envs")
+        next_state, reward, terminal = transition_triplet
+
+        return next_state, reward, terminal
+
+
+class MpiEnvWorker(MpiEnvComm):
+    """ MPI worker class that runs the environments.
+    """
+
+    def __init__(self, nenv, n_env_proc, env_fn, **env_kwargs):
+        super().__init__(n_env_proc)
+        self.nenv = nenv
+
+        # Initialize environments for per processes(double the given amount)
+        self._envs = [{"env": env_fn(**env_kwargs),
+                       "done": False}
+                      for i in range(nenv*2)]
+
+        self.observation_space = self._envs[0]["env"].observation_space
+        self.action_space = self._envs[0]["env"].action_space
+        self.active_slice = slice(0, nenv)
+
+        # Reset all the environments & start worker loop
+        self._states = np.stack([env["env"].reset() for env in self._envs])
+        self.transition = None
+
+        self.worker_loop()
+
+    def step(self, actions):
+        next_states, rewards, terminals, _ = [
+            np.stack(x, axis=0) for x in
+            zip(*[env.step(a.item()) for a, env in zip(actions, self.envs)])]
+
+        # Reset the environments if nececcessay
+        for ix, (done, n_state, env) in enumerate(zip(terminals,
+                                                      next_states,
+                                                      self.envs)):
+            if done.item() is True:
+                self.states[ix] = env.reset()
+            else:
+                self.states[ix] = n_state
+
+        return next_states, rewards, terminals
+
+    def _switch_envs(self):
+        if self.active_slice.start == 0:
+            self.active_slice = slice(self.nenv, self.nenv*2)
+        else:
+            self.active_slice = slice(0, self.nenv)
+
+    def receive_cmd(self):
+        """ Receive commands from gpu processes.
+        Docstring for commands is available in MpiEnv class.
+        """
+        cmd = self.remote_comm.bcast(None, root=0)
+        return cmd
+
+    def _s_state(self):
+        """ Send states of the last active environments.
+        """
+        gather_and_scatter(self.local_comm, self.states, self.remote_comm)
+
+    def _s_state_r_action(self):
+        """
+        """
+        actions = gather_and_scatter(
+            self.local_comm, self.states, self.remote_comm)
+        next_states, rewards, terminals = self.step(actions)
+        self.transition = (next_states, rewards, terminals)
+
+    def _s_trans_r_action(self):
+        """
+        """
+        actions = list_gather_and_scatter(
+            self.local_comm, self.transition, self.remote_comm)[0]
+        next_states, rewards, terminals = self.step(actions)
+        self.transition = (next_states, rewards, terminals)
+
+    def _s_trans(self):
+        """
+        """
+        list_gather_and_scatter(
+            self.local_comm, self.transition, self.remote_comm)
+
+    def worker_loop(self):
+        function_call_dict = {
+            0: self._s_state,
+            1: self._s_state_r_action,
+            2: self._s_trans_r_action,
+            3: self._s_trans,
+            4: self._switch_envs,
+        }
+        while True:
+            cmd = self.receive_cmd()
+            command = cmd["command"]
+            if command == 5:
+                exit()
+            job = function_call_dict[command]
+            job()
+
+    @property
+    def envs(self):
+        return [env["env"] for env in self._envs[self.active_slice]]
+
+    @property
+    def states(self):
+        return self._states[self.active_slice]
+
+
+def MpiEnv(nenv, n_env_proc, env_fn, **env_kwargs):
+    """ Fake class
+    """
+    color = MpiEnvComm.get_color(n_env_proc)
+
+    if color == 0:
+        return MpiEnvClient(nenv, n_env_proc, env_fn, **env_kwargs)
+    else:
+        return MpiEnvWorker(nenv, n_env_proc, env_fn, **env_kwargs)
+
+
+if __name__ == "__main__":
+    env = MpiEnv(nenv=20, n_env_proc=4,
+                 env_fn=lambda: gym.make("CartPole-v1"))
+    actions = np.ones((env.nenv * env.n_env_proc, 1), dtype=np.int32)
+
+    states = env.observe()
+    states = env.observe(actions)
+    next_s, reward, terminal = env.step(actions)
+    next_s, reward, terminal = env.step()
+
+    env.send_cmd("terminate")
+    print(reward)
--- doctrina/utils/mpi_utils.py
+++ doctrina/utils/mpi_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 def gather_and_scatter(local_comm, send_array, remote_comm):
     """ Combined MPI function that works similar to alltoall function. Gather
     the buffers locally and scatter them uniformly through an
-    inter-communicater. This functinality is only served for two groups.
+    inter-communicater. This functionality is only served for two groups.
 
     process | Group A | Group B | Local Root | Remote Root | Gather  | Scatter
     --------|---------|---------|------------|-------------|---------|--------
@@ -20,10 +20,10 @@ def gather_and_scatter(local_comm, send_array, remote_comm):
     calls used in this function are as follows:
     - bcast: Remote leaders broadcast the shape, #processes and dtype
     informations to the other group.
-    - Gather: Local leaders(rank 0 in local_comm) gathers buffers locally
+    - Gather: Local leaders(rank 0 in local_comm) gather buffers locally
     - Scatter: Remote leaders scatter the gathered data by uniformly dividing
     it to remote processes. Note that, gathered buffer must be divedable to
-    #remote processes.
+    # remote processes.
 
     Arguments:
         - local_comm: MPI communicater instance for the local group
@@ -46,8 +46,9 @@ def gather_and_scatter(local_comm, send_array, remote_comm):
     # <-------------- Get shape and dtype of remote ------------->
     # Local leader
     if local_rank == local_leader:
-        info = {"shape": len(send_array.flatten()),
+        info = {"dsize": len(send_array.flatten()),
                 "dtype": send_array.dtype,
+                "shape": send_array.shape,
                 "size": local_comm.Get_size()}
         remote_comm.bcast(info, MPI.ROOT)
     recvbuf_info = remote_comm.bcast(None, root=local_leader)
@@ -74,12 +75,128 @@ def gather_and_scatter(local_comm, send_array, remote_comm):
         remote_comm.Scatter(sendbuf.flatten(), None, root=MPI.ROOT)
 
     recvbuf = np.empty(
-        shape=((recvbuf_info["shape"] *
+        shape=((recvbuf_info["dsize"] *
                 recvbuf_info["size"]) // local_comm.Get_size()),
         dtype=recvbuf_info["dtype"])
     remote_comm.Scatter(None, recvbuf, root=0)
-    return recvbuf
+    return recvbuf.reshape(-1, *(recvbuf_info["shape"][1:]))
+
+
+def list_gather_and_scatter(local_comm, send_list, remote_comm):
+    """
+    """
+    if not isinstance(send_list, (tuple, list)):
+        raise ValueError("Send list must be a list or tuple. Currently {}"
+                         .format(type(send_list)))
+    # Gather data from local_comm
+    recv_list = [None] * len(send_list)
+    local_leader = 0
+    local_rank = local_comm.Get_rank()
+    # <-------------- Get shape and dtype of remote ------------->
+    # Local leader
+    if local_rank == local_leader:
+        info = {"size": local_comm.Get_size(),
+                "list": [{"dtype": arr.dtype,
+                          "dsize": np.product(arr.shape),
+                          "shape": arr.shape,
+                          } for arr in send_list]}
+        remote_comm.bcast(info, MPI.ROOT)
+    recvbuf_info = remote_comm.bcast(None, root=local_leader)
+
+    # Check for distributability
+    for arr in send_list:
+        if (np.product(arr.shape) * local_comm.Get_size() %
+                recvbuf_info["size"] != 0):
+            raise ValueError("Gathered data cannot be equally divided")
+
+    # <-------------- Gather send_list's to local leader ------------->
+    # Local leader
+    if local_rank == local_leader:
+        recv_list = []
+        for arr in send_list:
+            send_shape = arr.shape
+            local_proc_size = local_comm.Get_size()
+            recvbuf = np.empty((local_proc_size, *send_shape),
+                               dtype=arr.dtype)
+            recv_list.append(recvbuf)
+
+    # Local leader gathers the data from local processes of the group
+    for (r_arr, s_arr) in zip(recv_list, send_list):
+        local_comm.Gather(s_arr, r_arr, root=local_leader)
+
+    # Scatter gathered arrays
+    if local_rank == local_leader:
+        send_list = recv_list
+
+    # Local leader
+    if local_rank == local_leader:
+        for ix, s_arr in enumerate(recv_list):
+            remote_comm.Scatter(s_arr.flatten(), None, root=MPI.ROOT)
+
+    recv_list = []
+    for ix, arr_info in enumerate(recvbuf_info["list"]):
+        recvbuf = np.empty(
+            shape=((arr_info["dsize"] *
+                    recvbuf_info["size"]) // local_comm.Get_size()),
+            dtype=arr_info["dtype"]
+            )
+        remote_comm.Scatter(None, recvbuf, root=0)
+        recv_list.append(
+            recvbuf.reshape(-1, *(arr_info["shape"][1:]))
+        )
+
+    return recv_list
+
+
+def share_gradients(agent):
+    """ Gradients are shared among gpu processes.
+    """
+    raise NotImplementedError
+    sendbuf = torch.cat([p.grad for p in agent.parameters()
+                         if p.grad is not None])
+    recvbuf = torch.empty_like(sendbuf)
+    torch.distributed.all_reduce_multigpu([sendbuf, recvbuf], op=ReduceOp.SUM)
+    # Assign gradients
+    grad_sizes = [np.product(p.shape) for p in p.parameters()
+                  if p.grad is not None]
+    summed_grads = recvbuf.split(grad_size)
+    for p, grad in zip((p for p in agent.parameters() if p.grad is not None),
+                       summed_grad):
+        p.grad = summed_grads
+    raise RuntimeError("summed_grads must be divided by #gpus")
 
 
 if __name__ == "__main__":
-    pass
+
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
+
+    color = rank >= (size // 3)
+    local_comm = MPI.COMM_WORLD.Split(
+        color=color,
+        key=rank
+    )
+
+    local_rank = local_comm.Get_rank()
+    local_size = local_comm.Get_size()
+    remote_comm = local_comm.Create_intercomm(
+        local_leader=0,
+        peer_comm=MPI.COMM_WORLD,
+        remote_leader=0 if color == 1 else (size // 3),
+        tag=5
+    )
+
+    remote_leader = 0 if color == 1 else (size // 3),
+
+    if color == 0:
+        send_list = [np.ones((200, 1), dtype=np.int32)]
+    else:
+        send_list = [
+            np.ones((100, 4, 84, 84), dtype=np.uint8),
+            np.zeros(100, dtype=np.float32),
+            np.zeros(100, dtype=np.int32),
+        ]
+
+    recv = list_gather_and_scatter(
+        local_comm, send_list, remote_comm)
+    print([x.shape for x in recv], [x.shape for x in send_list], rank, remote_leader)
